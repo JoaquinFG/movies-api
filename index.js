3bfea165d348820fac77588199f9ef212b43e58f
Joaquin_FG <48914838+JoaquinFG@users.noreply.github.com>
2020-10-09 14:00:01 +0100

    implementing crud

--- index.js
+++ index.js
@@ -3,14 +3,10 @@ const app = express();
 
 const { config } = require('./config/index');
 
-app.get('/', function(req, res) {
-    res.send('hello world');
-});
+const modulesApi = require('./routes/movies.js');
 
-app.get('/json', function (req, res) {
-  res.send({ hello: 'world'});
-});
+modulesApi(app);
 
 app.listen(config.port, function () {
-    console.log(`Listening http://localhost:${config.port}`);
+  console.log(`Listening http://localhost:${config.port}`);
 });
